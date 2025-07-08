3f94ec78b178d056bdadec5a570c279b8a2c6968
Carlos Serrano <secarl77@gmail.com>
2025-07-07 22:35:25 -0400

    creating workflow

--- json-consumer/src/main/java/com/example/demo/DemoController.java
+++ json-consumer/src/main/java/com/example/demo/DemoController.java
@@ -1,4 +1,4 @@
-
+/*
 package com.example.demo;
 
 import c0917329.utils.JsonHelper;
@@ -9,3 +9,25 @@ public class DemoController {
         System.out.println(json);
     }
 }
+*/
+package com.example.demo;
+
+import c0917329.utils.JsonHelper;
+
+public class DemoController {
+    public static void main(String[] args) {
+        String result = JsonHelper.toJson(new Person("Carlos", 47));
+        System.out.println(result);
+    }
+
+    static class Person {
+        public String name;
+        public int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
+
